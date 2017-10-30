671de4bf07806dc497c093766fc9f738cbb3772d
Mayomi <mayomi@baka.wang>
2017-10-30 17:12:31 +0800

    Update InteractiveCapitolName.java
    增加中文名称支持

--- civcraft/src/com/avrgaming/civcraft/interactive/InteractiveCapitolName.java
+++ civcraft/src/com/avrgaming/civcraft/interactive/InteractiveCapitolName.java
@@ -49,7 +49,7 @@ public class InteractiveCapitolName implements InteractiveResponse {
 			return;
 		}
 		
-		if (!StringUtils.isAlpha(message) || !StringUtils.isAsciiPrintable(message)) {
+		if(!message.matches("[a-zA-Z\\u4E00-\\u9FA5]{2,5}")) {
 			CivMessage.send(player, CivColor.Rose+ChatColor.BOLD+CivSettings.localize.localizedString("interactive_capitol_invalidname"));
 			return;
 		}
